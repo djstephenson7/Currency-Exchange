5da96f7c0dbb7454581e2932e9737e08434b1449
djstephenson7 <dan_0502@hotmail.co.uk>
2019-05-06 16:53:47 +0100

    Switched to RSpec

--- spec/currency_exchange_spec.rb
+++ spec/currency_exchange_spec.rb
@@ -1,16 +1,16 @@
 require 'currency_exchange'
 require 'date'
 
-class DummyClass
-end
-
-describe 'Thing' do
-  before(:each) do
-    @dummy_class = DummyClass.new
-    @dummy_class.extend(CurrencyExchange)
+describe 'CurrencyExchange module rate calculator' do
+  it 'Gets the correct rate between GBP and USD on 22/11/2018' do
+    correct_rate = 1.2870493690602498
+    date = Date.new(2018, 11, 22)
+    expect(CurrencyExchange.rate(date, 'GBP', 'USD')).to eq correct_rate
   end
 
-  it 'get hello string' do
-    expect(@dummy_class.hello).to eq 'Hello'
+  it 'Gets the correct rate between GBP and USD on 22/11/2018' do
+    correct_rate = 0.007763975155279502
+    date = Date.new(2018, 11, 22)
+    expect(CurrencyExchange.rate(date, 'GBP', 'USD')).to eq correct_rate
   end
 end
