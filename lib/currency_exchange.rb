367e35ea8d8c0ee0f762f26ee890edea88cbe868
djstephenson7 <dan_0502@hotmail.co.uk>
2019-05-07 11:10:16 +0100

    Raises exception if currency not found

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -6,15 +6,12 @@ module CurrencyExchange
   # Return the exchange rate between from_currency and to_currency on date as a float.
   # Raises an exception if unable to calculate requested rate.
   # Raises an exception if there is no rate for the date provided.
-  # TODO: calculate and return rate
   def self.rate(date, from_currency, to_currency)
     date = date.strftime('%Y-%m-%d')
-    first_rate = @data_hash[date][from_currency]
-    second_rate = @data_hash[date][to_currency]
-    p second_rate / first_rate
-    # Iterate over @data_hash
-    # Match date to date in JSON
-    # If it matches, calculatedifference between 2nd and 3rd arg
-    # FORMULA: to_currency / from_currency
+    if !@data_hash[date][to_currency] || !@data_hash[date][from_currency]
+      raise 'Currency not found'
+    else
+      @data_hash[date][to_currency] / @data_hash[date][from_currency]
+    end
   end
 end
--- spec/currency_exchange_spec.rb
+++ spec/currency_exchange_spec.rb
@@ -8,15 +8,21 @@ describe 'CurrencyExchange module rate calculator' do
     expect(CurrencyExchange.rate(date, 'GBP', 'USD')).to eq correct_rate
   end
 
+  it 'Gets the correct rate between CZK and AUD on 10/12/2018' do
+    correct_rate = 0.061246423876904044
+    date = Date.new(2018, 12, 10)
+    expect(CurrencyExchange.rate(date, 'CZK', 'AUD')).to eq correct_rate
+  end
+
   it 'Gets the correct rate between GBP and USD on 22/11/2018' do
     correct_rate = 1.2870493690602498
     date = Date.new(2018, 11, 22)
     expect(CurrencyExchange.rate(date, 'GBP', 'USD')).to eq correct_rate
   end
 
-  xit 'Gets the correct rate between JPY and EUR on 22/11/2018' do
-    correct_rate = 0.007763975155279502
+  it 'Raises an exception if currency is not found on the date' do
     date = Date.new(2018, 11, 22)
-    expect(CurrencyExchange.rate(date, 'JPY', 'EUR')).to eq correct_rate
+    expect { CurrencyExchange.rate(date, 'JPY', 'EUR') }
+      .to raise_error('Currency not found')
   end
 end
